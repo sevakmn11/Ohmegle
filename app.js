350aad3fd8b05832419df8a3665de282b12d8725
Sevak Mnatsakanyan <sevakmn11@gmail.com>
2023-11-25 19:33:34 -0800

    btn

--- app.js
+++ app.js
@@ -346,7 +346,7 @@ wss.on('connection', (ws, req) => {
     )
 
     if (ws.peer) {
-      var content =  {self: ws.socket.remoteAddress + ":" + self.socket.remotePort, other: ws.peer.socket.remoteAddress + ":" + ws.peer.socket.remotePort};
+      var content =  {self: ws.socket.remoteAddress + ":" + ws.socket.remotePort, other: ws.peer.socket.remoteAddress + ":" + ws.peer.socket.remotePort};
       console.log("content in close: ", content) 
       ws.peer.send(JSON.stringify({ channel: 'disconnect', data: content }));
       ws.peer.peer = undefined
