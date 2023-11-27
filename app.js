cdeee98f3f120563abfac3ab3f894d625b1183fb
Sevak Mnatsakanyan <sevakmn11@gmail.com>
2023-11-26 16:39:10 -0800

    ai

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ import * as https from 'https';
 import * as http from 'http';
 import { OpenAI } from 'openai';
 
-const openai = new OpenAI('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');
+const openai = new OpenAI({apiKey: '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'});
 
 var privateKey = fs.readFileSync('public/private.key', 'utf8');
 var certificate = fs.readFileSync('public/certificate.crt', 'utf8');
