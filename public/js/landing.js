e2f80b5b2f730bdd6522651659e6bd710860829f
Sevak Mnatsakanyan <sevakmn11@gmail.com>
2023-11-27 17:17:36 -0800

    hi

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,13 +1,3 @@
-const $ = (x) => document.querySelector(x)
-const $$ = (x) => document.querySelectorAll(x)
-const esc = (x) => {
-  const txt = document.createTextNode(x)
-  const p = document.createElement('p')
-  p.appendChild(txt)
-  return p.innerHTML
-}
-let base = Math.floor(Math.random() * 50 + 30)
-const noise = Math.floor(Math.random() * 10 - 5)
 const $ = (x) => document.querySelector(x);
 const $$ = (x) => document.querySelectorAll(x);
 const esc = (x) => {
@@ -113,57 +103,3 @@ async function getPeopleOnline() {
 configureTags();
 window.addEventListener("load", initTagsFromURL);
 await getPeopleOnline();
-if (!sessionStorage.getItem('peopleOnline')) {
-  sessionStorage.setItem('peopleOnline', base)
-} else {
-  base = +sessionStorage.getItem('peopleOnline')
-}
-
-function configureTags() {
-  const $input = $('#interest-container input')
-  const $tags = $('#tag-container')
-  const $textBtn = $('#text-btn')
-  const $videoBtn = $('#video-btn')
-
-  $input.addEventListener('keydown', (e) => {
-    if (e.key !== 'Enter' && e.key !== ',') return
-
-    const value = $input.value.trim()
-    if (!value) return
-
-    const tag = document.createElement('div')
-    tag.id = 'tag'
-    tag.innerHTML = `<p><span>${esc(value)}</span> ×</p>`
-    tag.style = 'cursor: pointer'
-    tag.onclick = () => tag.remove()
-    $tags.appendChild(tag)
-    $input.value = ''
-
-    e.preventDefault()
-  })
-
-  $textBtn.addEventListener('click', () => {
-    const interests = Array.from($$('#tag p span')).map((x) => x.innerText)
-    window.location.href = '/chat?' + new URLSearchParams({ interests })
-  })
-
-  $videoBtn.addEventListener('click', () => {
-    const interests = Array.from($$('#tag p span')).map((x) => x.innerText)
-    window.location.href = '/video?' + new URLSearchParams({ interests })
-  })
-}
-
-async function getPeopleOnline() {
-  const $peopleOnline = $('#peopleOnline p span')
-  const res = await fetch('/online')
-  if (!res.ok) {
-    return console.error("Couldn't fetch GET /online")
-  }
-  const { online } = await res.json()
-  console.log(online)
-
-  $peopleOnline.innerHTML = base + noise + +online
-}
-
-configureTags()
-await getPeopleOnline()
