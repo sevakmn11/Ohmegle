3c93fa190053418de904363d5b402a2240406b38
Sevak Mnatsakanyan <sevakmn11@gmail.com>
2023-12-05 19:36:08 -0800

    shema

--- public/js/main.js
+++ public/js/main.js
@@ -7,43 +7,4 @@ function configureTopbar() {
   })
 }
 
-// function configureFeedbackModal() {
-//   const $modal = $('#feedbackModal')
-//   const $btn = $('#feedback-btn')
-//   const $close = $('#feedbackModal .close')
-//   const $text = $('#feedbackText')
-
-//   $btn.onclick = () => {
-//     $modal.style.display = 'block'
-//   }
-
-//   $close.onclick = () => {
-//     $modal.style.display = 'none'
-//   }
-
-//   window.onclick = (e) => {
-//     if (e.target == $modal) {
-//       $modal.style.display = 'none'
-//     }
-//   }
-
-//   $('#feedbackForm').onsubmit = async (e) => {
-//     e.preventDefault()
-//     let feedback = $text.value
-//     $text.value = ''
-//     $modal.style.display = 'none'
-//     // send feedback to backend
-//     await fetch('/feedback', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       body: JSON.stringify({
-//         feedback,
-//       }),
-//     })
-//   }
-// }
-
 configureTopbar()
-// configureFeedbackModal()
